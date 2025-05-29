e47d2fddc8c3674d9298f19cfb4b090b0886caff
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-05-29 22:58:00 +0200

    feat: add artist resourse

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), UserModule, TrackModule],
+  }), UserModule, TrackModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
