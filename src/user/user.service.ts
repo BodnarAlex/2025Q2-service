7910ca4264ac1a71b1b883c41ecfaf13021bc4b8
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-05-30 17:06:12 +0200

    feat: add user entity and service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { randomUUID } from 'crypto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  private users: User[] = [];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const time = Date.now();
+    const newUser: User = {
+      login: createUserDto.login,
+      password: createUserDto.password,
+      id: randomUUID,
+      version: 1,
+      createdAt: time,
+      updatedAt: time,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const user = this.users.filter((u: User) => u.id === String(id)).at(0);
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user = this.users.filter((u: User) => u.id === String(id)).at(0);
+    user.password = updateUserDto.newPassword;
+    user.version++;
+    user.updatedAt = Date.now();
+    return user;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const index = this.users.findIndex((u: User) => u.id === String(id));
+    this.users.splice(index, 1);
   }
 }
