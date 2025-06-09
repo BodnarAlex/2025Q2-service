c03155acd031ba5261fca55e7b2c0151037166fa
Alexsandra Bodnar <allugovskova@mail.ru>
2025-06-09 14:34:26 +0200

    fix: rewrite for nomedon for restart after crash

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ import { FavoritesModule } from './favorites/favorites.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+throw new Error('t');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
